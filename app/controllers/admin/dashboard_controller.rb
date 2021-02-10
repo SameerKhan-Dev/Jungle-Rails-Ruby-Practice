93d2ad081d748fcf5365f1b60dce7ac40636e4a5
SameerKhan-Dev <sameerk_786@hotmail.com>
2021-02-10 19:46:40 +0000

    added total products count and total categories count onto dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
 
   puts http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @totalProductsCount = Product.count
+    @totalCategoriesCount = Category.count
   end
 end
